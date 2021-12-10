f0d20d84e68a0d7ff8004f3c3707a580044c4bf6
Rich Seibert <rs2585@cornell.edu>
2021-12-10 00:27:28 -0500

    added signals of interest file (freqs where strong signals are found using SDR), and add more shit to project

--- aml/project/aml_project.py
+++ aml/project/aml_project.py
@@ -12,6 +12,7 @@ TODO:
     - Finish CNN and random forests
     - Add dev/hold-out sets
     - Redo performance of svm with more data
+    - Do tests with white noise added
     - start with simple model, train/dev/test, print metrics (lecture 20 p2/3)
       and plots, evalutate whats going on (overfitting, underfitting) and
       then try different things and improve model. Then move on to more
@@ -76,15 +77,16 @@ def formatIQTimeData(fileNames):
         dataPart = dataPart[1024*5:]
         # TODO try spectrogram/fft here and use freq data to train the model
         # reshape into 2d array
-        wrapSize = 1024
-        for i in range(0, len(dataPart), wrapSize):
+        wrapSize = 4096
+        rowsAdded = 0
+        for i in range(0, len(dataPart)-wrapSize, wrapSize):
+            rowsAdded += 1
             data.append(dataPart[i:i+wrapSize])
         # label is first few characters on file that indicates modulation type
         fileNameStart = fileName.rfind('/')+1
         # cut the entire file path out, just keep file name
         fileNameOnly = fileName[fileNameStart:]
         label = fileNameOnly[:fileNameOnly.find('_')]
-        rowsAdded = int(len(dataPart)/wrapSize)
         targets = np.concatenate((targets, np.array([label]*rowsAdded)))
     return np.array(data), targets
 
@@ -142,8 +144,12 @@ def CNN(fileNames):
     targets = le.transform(targets)
 
     reshaped = []
+    # this is the length of on row
+    dataCutSize = data[0].shape[0]
+    sqSize = int(dataCutSize**0.5)
     for i in range(data.shape[0]):
-        reshaped.append(data[i].reshape(32, 32))
+        if data[i].shape[0] == dataCutSize:
+            reshaped.append(data[i].reshape(sqSize, sqSize))
     data = np.array(reshaped)
     
     # Split data into 50% train and 50% test subsets
@@ -158,15 +164,15 @@ def CNN(fileNames):
     model = Sequential()
     # Convolution layer
     model.add (
-        Conv2D(32, (3, 3),
+        Conv2D(sqSize, (3, 3),
         activation = 'relu', kernel_initializer = 'he_uniform' ,
-        input_shape = (32 , 32 , 1 ))
+        input_shape = (sqSize , sqSize , 1 ))
     )
     # 2nd Convolution layer
     model.add (
-        Conv2D(64, (3, 3),
+        Conv2D(sqSize*2, (3, 3),
         activation = 'relu', kernel_initializer = 'he_uniform' ,
-        input_shape = (32 , 32 , 1 ))
+        input_shape = (sqSize , sqSize, 1 ))
     )
     # Maxpooling layer
     model.add(MaxPooling2D((2, 2)))
@@ -202,7 +208,7 @@ def CNN(fileNames):
         x_train,
         y_train,
         batch_size=32,
-        epochs=1,
+        epochs=3,
         validation_data=(x_test, y_test)
     )
     
@@ -230,24 +236,34 @@ def randomForest(fileNames):
 
 
 def SVM(fileNames):
-    print("Training and testing SVM model classification for the following signals:", labels)
-
     # reformat data recorded by sdr into 2d array where each row is 
     # one array of one type of signal data. If multiple files
     # are input it will concatenate all of them together
     data, targets = formatIQTimeData(fileNames)
+
+    # for svm, remove a bunch of data, no need to feed in all the data 
+    # takes too long to train
+    n = 8
+    cutData = data[::n]
+    cutTargets = targets[::n]
+
+    print("Training and testing SVM model classification for the following signals:", set(targets))
     
     # Split data into 50% train and 50% test subsets
     X_train, X_test, y_train, y_test = train_test_split(
-        data, targets, test_size=0.5, shuffle=True, random_state=42)
-    
+        cutData, cutTargets, test_size=0.3, shuffle=True, random_state=30)
+
     # Create a classifier: a support vector classifier
+    print("Create SVM")
     clf = svm.SVC(gamma=0.001)
     
+    print(X_train.shape, y_train.shape)
     # Learn the digits on the train subset
+    print("Fit data")
     clf.fit(X_train, y_train)
     
     # Predict the value of the digit on the test subset
+    print("Predict")
     predicted = clf.predict(X_test)
     
     print(
@@ -255,46 +271,52 @@ def SVM(fileNames):
         f"{metrics.classification_report(y_test, predicted)}\n")
 
 
-def record(modId, num1k=4096, centerFreq, plot):
-    num1k = int(num1k)
-    plot = int(plot)
-    # capture signal from rtl sdr
-    sdr = RtlSdr()
-    # 200 KHz for FM, 20 for AM
-    fs = 1e6
-    sdr.sample_rate = fs
-    sdr.center_freq = float(centerFreq)*1e6
-    sdr.gain = 'auto'
-    
-    samples = sdr.read_samples(num1k*1024)
-    sdr.close()
-    
-    fileName =  modId + '_' + centerFreq + '_' + string_fs + \
-                '_' + 'raw_data.npy', samples
-    
-    print("Writting Data")
-    # write raw iq data to modify later if needed
-    string_fs = str(fs/1e6)+'MHz'
-    np.save(fileName)
-    
-    # really no reason to save off spectrogram...
-    saveSpec = False
-    if saveSpec:
-        # write normalized spectrogram mag data
-        f, t, Sxx = signal.spectrogram(samples, fs) #nfft=1024, defaults to 256
-        dataSpectrogram = fftshift(Sxx, axes=0)
-        # normalize to 0-1
-        dataSpectrogram = dataSpectrogram / np.max(dataSpectrogram)
-        #dataSpectrogram = 10*np.log10(dataSpectrogram)
-        np.save(modId + '_' + centerFreq + '_' + string_fs + '_' + 
-               'norm_spec_data.npy', dataSpectrogram)
-    
-    if plot:
-        plot(samples)
+def record(modId, num1k, centerFreq, plot):
+    for i in range(3):
+        num1k = int(num1k)
+        plot = int(plot)
+        # capture signal from rtl sdr
+        sdr = RtlSdr()
+        # 200 KHz for FM, 20 for AM
+        fs = 1e6
+        sdr.sample_rate = fs
+        sdr.center_freq = float(centerFreq)*fs
+        sdr.gain = 'auto'
+        
+        samples = sdr.read_samples(num1k*1024)
+        sdr.close()
+        
+        string_fs = str(fs/1e6)+'MHz'
+        fileName =  modId + '_' + centerFreq + '_' + string_fs + \
+                    '_' + 'raw_data' + str(i) + '.npy'
+        
+        print("Writing Data")
+        # write raw iq data to modify later if needed
+        np.save(fileName, samples)
+        
+        '''
+        # really no reason to save off spectrogram...
+        saveSpec = False
+        if saveSpec:
+            # write normalized spectrogram mag data
+            f, t, Sxx = signal.spectrogram(samples, fs) #nfft=1024, defaults to 256
+            dataSpectrogram = fftshift(Sxx, axes=0)
+            # normalize to 0-1
+            dataSpectrogram = dataSpectrogram / np.max(dataSpectrogram)
+            #dataSpectrogram = 10*np.log10(dataSpectrogram)
+            np.save(modId + '_' + centerFreq + '_' + string_fs + '_' + 
+                   'norm_spec_data.npy', dataSpectrogram)
+        '''
+        
+        if plot:
+            plt.specgram(samples, Fs=1e6, cmap="rainbow")
+            plt.title('Spectrogram')
+            plt.xlabel("Time (s)")
+            plt.ylabel("IF (MHz)")
+            plt.show()
 
 
 if __name__ == "__main__":
-    
     if len(sys.argv) <= 1:
         print("How to use this script:\n")
         print("Example: " +
@@ -312,7 +334,6 @@ if __name__ == "__main__":
     else:
         path = sys.argv[2]
         fileNames = glob.glob(path + "/*.npy")
-        print("Files being ran:", fileNames)
         if sys.argv[1] == 'svm':
             SVM(fileNames)
         elif sys.argv[1] == 'cnn':
@@ -323,3 +344,5 @@ if __name__ == "__main__":
             fileNames = sys.argv[2:]
             for fileName in fileNames:
                 plot(fileName)
+        else:
+            print("No argument matches your input")
